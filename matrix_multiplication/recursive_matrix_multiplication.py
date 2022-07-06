4cd64a7bdbd7e5f480710311f3b99b2c484c1c3c
ranjanikrishnan <ranjanikrishnanr@gmail.com>
2022-07-06 10:49:34 +0200

    split into smaller functions

--- matrix_multiplication/recursive_matrix_multiplication.py
+++ matrix_multiplication/recursive_matrix_multiplication.py
@@ -9,17 +9,8 @@ def recursive_matrix_multiplication(x: List[List[int]], y: List[List[int]]) -> L
         z[0][0] = x[0][0] * y[0][0]
         return z
 
-    # a, b, c, d = get_submatrices(x, n)
-    # e, f, g, h = get_submatrices(y, n)
-
-    a = [x[i][:n//2] for i in range(n//2)]
-    b = [x[i][n//2:] for i in range(n//2)]
-    c = [x[i][:n//2] for i in range(n//2, n)]
-    d = [x[i][n//2:] for i in range(n//2, n)]
-    e = [y[i][:n//2] for i in range(n//2)]
-    f = [y[i][n//2:] for i in range(n//2)]
-    g = [y[i][:n//2] for i in range(n//2, n)]
-    h = [y[i][n//2:] for i in range(n//2, n)]
+    a, b, c, d = get_submatrices(x, n)
+    e, f, g, h = get_submatrices(y, n)
 
     p1 = recursive_matrix_multiplication(a, e)
     p2 = recursive_matrix_multiplication(b, g)
@@ -35,6 +26,30 @@ def recursive_matrix_multiplication(x: List[List[int]], y: List[List[int]]) -> L
     term_3 = add_matrix(p5, p6)
     term_4 = add_matrix(p7, p8)
 
+    z = combine_submatrices(term_1, term_2, term_3, term_4, n, z)
+    return z
+
+
+def get_submatrices(x, n):
+    a = [x[i][:n//2] for i in range(n//2)]
+    b = [x[i][n//2:] for i in range(n//2)]
+    c = [x[i][:n//2] for i in range(n//2, n)]
+    d = [x[i][n//2:] for i in range(n//2, n)]
+
+    return a, b, c, d
+
+
+def add_matrix(a1, a2):
+    added = []
+    for i in range(len(a1)):
+        result = []
+        for j in range(len(a1[0])):
+            result.append(a1[i][j]+a2[i][j])
+        added.append(result)
+    return added
+
+
+def combine_submatrices(term_1, term_2, term_3, term_4, n, z):
     for i in range(n//2):
         for j in range(n//2):
             z[i][j] = term_1[i][j]
@@ -52,23 +67,3 @@ def recursive_matrix_multiplication(x: List[List[int]], y: List[List[int]]) -> L
             z[i][j] = term_4[i-n//2][j-n//2]
 
     return z
-
-
-def add_matrix(a1, a2):
-    added = []
-    for i in range(len(a1)):
-        result = []
-        for j in range(len(a1[0])):
-            result.append(a1[i][j]+a2[i][j])
-        added.append(result)
-    return added
-
-
-def combine_submatrices():
-    pass
-# def get_submatrices(x, n):
-#     a = [x[i][:n/2] for i in range(n/2)]
-#     a = [[0 for _ in range(n/2)] for _ in range(n/2)]
-#     b = [[0 for _ in range(n/2)] for _ in range(n/2)]
-#     c = [[0 for _ in range(n/2)] for _ in range(n/2)]
-#     d = [[0 for _ in range(n/2)] for _ in range(n/2)]
