fca0ac93834ca120350bb5a6bdfc53663fc6c0ad
ranjanikrishnan <ranjanikrishnanr@gmail.com>
2022-07-21 10:47:41 +0200

    code cosmetics

--- linear_time_selection/rselect.py
+++ linear_time_selection/rselect.py
@@ -4,15 +4,16 @@ import random
 
 def rselect(a: List[int], order: int):
     required_index = order - 1
-    if len(a) == 1:
+    n = len(a)
+    if n == 1:
         return a[0]
     l = 0
-    r = len(a)
-    p = random.randint(l, r - 1)
+    r = n - 1
+    p = random.randint(l, r)
     a[l], a[p] = a[p], a[l]
     p = l
     i, j = p+1, p+1
-    while j <= len(a)-1:
+    while j <= n - 1:
         if a[j] <= a[p]:
             a[i], a[j] = a[j], a[i]
             i += 1
